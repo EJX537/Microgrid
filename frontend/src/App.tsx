7420f4fe02e1513b7e5588385e2647d20ecba388
EJX537 <meisericxie@gmail.com>
2023-10-21 17:27:38 -0700

    Comments

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { Layout, theme } from 'antd';
+import { Layout } from 'antd';
 
 import Sidebar from './components/sidebar';
 import HeaderContent from './components/header_content';
@@ -8,23 +8,20 @@ import ExampleGraph from './components/sample_graph';
 
 const { Header, Content, Footer, Sider } = Layout;
 const App: React.FC = () => {
+  // Collapsable Sidebar
   const [collapsed, setCollapsed] = useState(false);
-
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
 
   return (
     <Layout hasSider>
-      <Sider className='!fixed !h-screen' collapsible collapsed={collapsed} trigger={null}>
+      <Sider className='!fixed !h-screen' collapsible collapsed={collapsed} onCollapse={() => setCollapsed(!collapsed)}>
         <Sidebar collapsed={collapsed} />
       </Sider>
       <Layout style={{ transition: 'margin-left .2s', marginLeft: collapsed ? 80 : 200 }} className='min-h-screen min-w-[600px]'>
         <Header className='p-0 bg-white w-full z-50 fixed'>
           <HeaderContent collapsed={collapsed} setCollapsed={setCollapsed} />
         </Header>
-        <Content className='flex-grow mx-5 mt-16 h-full'>
-          <div style={{ background: colorBgContainer }} className='text-center p-6 pt-8 h-full flex flex-row flex-wrap justify-around'>
+        <Content className='flex-grow mx-5 mt-16 h-full bg-white'>
+          <div className='text-center p-6 pt-8 h-full flex flex-row flex-wrap justify-around'>
             <div className='w-[500px] h-[400px] mb-4 bg-gray-200 rounded-lg p-8'>
               <div className='h-[350px]'>
                 <ExampleGraph />
--- frontend/src/components/sample_graph.tsx
+++ frontend/src/components/sample_graph.tsx
@@ -4,6 +4,7 @@ import { Line } from '@ant-design/plots';
 const ExampleGraph = () => {
   const [data, setData] = useState([]);
 
+  // Exmaple Data
   const asyncFetch = () => {
     fetch('https://gw.alipayobjects.com/os/bmw-prod/1d565782-dde4-4bb6-8946-ea6a38ccf184.json')
       .then((response) => response.json())

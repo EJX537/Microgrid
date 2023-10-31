9960dcc94c1d8a3287f6168266645ee734a782ea
Kevin <kesparik@ucsc.edu>
2023-10-30 20:44:35 -0700

    Starting on the settings page

--- frontend/src/components/routes/routes.tsx
+++ frontend/src/components/routes/routes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from '../pages/dashboard/dashboard';
+import DeviceOneSettings from '../pages/dashboard/deviceOneSettings'
 import Page404 from '../page404';
 
 const RoutesProvider = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Dashboard />} />
+			<Route path = "/b" element={<DeviceOneSettings/>} />
 			<Route path="*" element={<Page404 />} />
 		</Routes>
 	);

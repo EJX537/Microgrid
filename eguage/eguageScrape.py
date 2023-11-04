52272ef320b547b764331779621f62c3372c668d
Peter Liu <xliu267@ucsc.edu>
2023-11-04 11:19:20 -0700

    first version of eguage scrape finished

---
+++ eguage/eguageScrape.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Create a new instance of the Firefox driver
+driver = webdriver.Firefox()
+
+# Open the website
+driver.get("http://egauge18646.egaug.es/")
+
+time.sleep(30)
+# Find the button element and click it
+button = driver.find_element(
+    By.ID, "graph_menu_button"
+)  # Replace with the actual button ID or other selector
+button.click()
+
+button = driver.find_element(
+    By.XPATH,
+    "//div[@class='GadgetMenuItem' and contains(text(), 'Export data to spreadsheet (CSV)')]",
+)
+button.click()
+
+button = driver.find_element(By.XPATH, "//input[@type='button' and @value='Export']")
+button.click()
+
+# Optionally, perform other tasks like filling out forms, navigating, or scraping data
+
+# Close the browser when done
+driver.quit()
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Set the path to your GeckoDriver executable
+driver_path = "./drivers/geckodriver"
+
+# URL of the website you want to scrape
+url = "http://egauge18646.egaug.es/"
+
+# Initialize the Firefox WebDriver
+driver = webdriver.Firefox(executable_path=driver_path)
+
+# Open the website
+driver.get(url)
+
+try:
+    # Wait for the button to be clickable
+    button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.ID, "graph_menu_button"))
+    )
+
+    # Click the button to open the dropdown menu
+    button.click()
+
+    # Wait for the download button to be clickable in the dropdown menu
+    button2 = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (
+                By.XPATH,
+                "//div[@class='GadgetMenuItem' and contains(text(), 'Export data to spreadsheet (CSV)')]",
+            )
+        )
+    )
+
+    # Click the download button to download the file
+    button2.click()
+
+    # Wait for the download button to be clickable in the dropdown menu
+    download_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable(
+            (By.XPATH, "//input[@type='button' and @value='Export']")
+        )
+    )
+
+    # Click the download button to download the file
+    download_button.click()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    # Close the browser window
+    driver.quit()

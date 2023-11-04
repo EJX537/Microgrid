96cc2a5dddf4e39fc655d55fee3f06161c7a8421
Peter Liu <xliu267@ucsc.edu>
2023-11-04 11:25:12 -0700

    deleted excess code

--- eguage/eguageScrape.py
+++ eguage/eguageScrape.py
@@ -1,37 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-
-# Create a new instance of the Firefox driver
-driver = webdriver.Firefox()
-
-# Open the website
-driver.get("http://egauge18646.egaug.es/")
-
-time.sleep(30)
-# Find the button element and click it
-button = driver.find_element(
-    By.ID, "graph_menu_button"
-)  # Replace with the actual button ID or other selector
-button.click()
-
-button = driver.find_element(
-    By.XPATH,
-    "//div[@class='GadgetMenuItem' and contains(text(), 'Export data to spreadsheet (CSV)')]",
-)
-button.click()
-
-button = driver.find_element(By.XPATH, "//input[@type='button' and @value='Export']")
-button.click()
-
-# Optionally, perform other tasks like filling out forms, navigating, or scraping data
-
-# Close the browser when done
-driver.quit()
-
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC

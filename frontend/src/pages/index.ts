9e19a6a1387b0c758544c4a8e0779f275c57aee1
EJX537 <meisericxie@gmail.com>
2023-11-05 17:30:08 -0800

    Merge Fix

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,4 +1,6 @@
 import Dashboard from './dashboard/dashboard';
 import Page404 from './page404/page404';
+import DeviceOneSettings from './dashboard/deviceOneSettings';
 
-export {Dashboard, Page404};
+
+export {Dashboard, Page404, DeviceOneSettings};
--- frontend/src/routes/routes.tsx
+++ frontend/src/routes/routes.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Dashboard from '../pages/dashboard/dashboard';
-import Page404 from '../page404';
-import DeviceOneSettings from '../pages/dashboard/deviceOneSettings';
+import { Dashboard, DeviceOneSettings, Page404 } from '../pages';
 
 const RoutesProvider = () => {
 	return (

ca30c1069da058c79717e47e003879e61472b958
Kevin Parikh <kesparik@ucsc.edu>
2023-11-13 12:20:19 -0800

    DB works perfect, just need to loop it

--- micro-services/eGauge/egauge_API_db.py
+++ micro-services/eGauge/egauge_API_db.py
@@ -10,11 +10,12 @@ import json
 
 # MySQL connection details
 mysql_config = {
-    'host': 'localhost',
-    'user': 'microgridManager',
-    'password': 'sluggrid',
-    'database': 'microgridManager',
+    "host": "localhost",
+    "user": "microgridManager",
+    "password": "sluggrid",
+    "database": "microgridManager",
 }
+
 
 # Function to create a MySQL connection
 def create_connection():
@@ -27,12 +28,13 @@ def create_connection():
         print(f"Error: {e}")
         return None
 
+
 # Function to insert data into MySQL table, handling duplicates
 def insert_data(connection, table_name, columns, values):
     try:
         cursor = connection.cursor()
         insert_query = f"INSERT INTO {table_name} ({', '.join(columns)}) VALUES ({', '.join(['%s']*len(columns))})"
-        
+
         # Insert each row individually
         for row_values in values:
             cursor.execute(insert_query, row_values)
@@ -48,14 +50,14 @@ def insert_data(connection, table_name, columns, values):
     finally:
         cursor.close()
 
+
 # Specify eGauge details
 meter_dev = os.getenv("EGDEV", "http://egauge18646.egaug.es")
 meter_user = os.getenv("EGUSR", "ppridge1")
 meter_password = os.getenv("EGPWD", "ppridge")
 
-dev = webapi.device.Device(
-    meter_dev, webapi.JWTAuth(meter_user, meter_password)
-)
+dev = webapi.device.Device(meter_dev, webapi.JWTAuth(meter_user, meter_password))
+
 
 def format_measurements(sensor_name: str, pq_list: List[PhysicalQuantity]):
     """Format a SENSOR_NAME and a list of physical quantities PQ_LIST such
@@ -68,6 +70,7 @@ def format_measurements(sensor_name: str, pq_list: List[PhysicalQuantity]):
             formatted = (18 - len(formatted)) * " " + formatted
         ret += formatted
     return ret
+
 
 # Request values for all available sensors: built-in environmental
 # sensors, all line inputs, and all sensors.  Note that, except for
@@ -99,7 +102,9 @@ local = Local(dev, query_string)
 print("string version", str(local))
 string_version = str(local)
 # THIS IS WHAT DATA THE SQL QUERIES WILL LOOP THROUGH
-data = json.loads(string_version)  # You can adjust the 'indent' parameter for formatting
+data = json.loads(
+    string_version
+)  # You can adjust the 'indent' parameter for formatting
 # print("check that we got it to become a json", type(json_string));
 # new_dict = dict(json_string) # make it back into a dictionary
 
@@ -117,80 +122,157 @@ print(rate)
 
 # Connect to MySQL (replace placeholders with actual values)
 db_config = {
-    'host': 'localhost',
-    'user': 'microgridManager',
-    'password': 'sluggrid',
-    'database': 'microgridManager',
+    "host": "localhost",
+    "user": "microgridManager",
+    "password": "sluggrid",
+    "database": "microgridManager",
 }
 
 # Connect to MySQL database
 connection = mysql.connector.connect(**db_config)
 cursor = connection.cursor()
 
+
 # Function to insert or update data in the table
-def insert_or_update(table_name, nested_data, column_names):
-    print("THIS IS WHAT NESTED_DATA IS\n", nested_data)
-    print("This is the type of nested_data\n", type(nested_data))
-    for key, value in nested_data.items():
-        # If the value is not a dictionary, convert it into a dictionary with a single key
-        if not isinstance(value, dict):
-            value = {key: value}
+def insert_or_update_rate(table_name, nested_data, column_names):
+    # Wrap column names with backticks to handle special characters
+    columns = ", ".join(
+        [
+            f"`{col.replace('*', '_').replace('-', 'neg').replace('+', 'pos').replace('/', 'div').replace(' ', '_')}`"
+            for col in column_names
+            if col != "time"
+        ]
+    )
 
-        # Wrap column names with backticks to handle special characters
-        columns = ', '.join([f"`{col.replace('*', '_').replace('-', 'neg').replace('+', 'pos').replace('/', 'div').replace(' ', '_')}`" for col in column_names if col != 'time'])
-        values = ', '.join(['%s' for _ in value.values()])
-        update_set = ', '.join([f'{column} = VALUES({column})' for column in column_names if column != 'time'])
+    # Create values
+    values = ", ".join(["%s"] * len(column_names))
+    update_set = ", ".join(
+        [
+            f"{column} = VALUES({column})"
+            for column in column_names
+            if column != "time"
+        ]
+    )
 
-        # Include the 'time' column separately in values and update_set
-        # Include the 'time' column separately in values and update_set
-        columns += ', time'
-        values += ', ' + ', '.join(['%s'] * len(value.keys()))
-        update_set += ', time = VALUES(time)'
+    # Include the 'time' column separately in values and update_set
+    columns += ", time"
+    values += ", %s"
+    update_set += ", time = VALUES(time)"
 
-        # Use ON DUPLICATE KEY UPDATE to handle updates
-        query = f"INSERT INTO {table_name} ({columns}) VALUES ({values}) ON DUPLICATE KEY UPDATE {update_set}"
+    # Use ON DUPLICATE KEY UPDATE to handle updates
+    # query = f"INSERT INTO {table_name} ({columns}) VALUES ({values}) ON DUPLICATE KEY UPDATE {update_set}"
+    query = f"INSERT INTO rate (`S4_L2`, `S3_L1`, `S6_L2`, `S5_L1`, `S2_L2`, `S1_L1`, `S8_L2`, `S7_L1`, `S10_L2`, `S9_L1`, `S12_L2`, `S11_L1`, time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE S4_L2 = VALUES(S4_L2), S3_L1 = VALUES(S3_L1), S6_L2 = VALUES(S6_L2), S5_L1 = VALUES(S5_L1), S2_L2 = VALUES(S2_L2), S1_L1 = VALUES(S1_L1), S8_L2 = VALUES(S8_L2), S7_L1 = VALUES(S7_L1), S10_L2 = VALUES(S10_L2), S9_L1 = VALUES(S9_L1), S12_L2 = VALUES(S12_L2), S11_L1 = VALUES(S11_L1), time = VALUES(time)"
 
-        # Print the SQL query
-        print("SQL Query:", query)
+    print("*******THIS IS MY QUERY ******\n")
+    print(query)
+    print("****THIS IS MY QUERY ******\n")
+    # Extract the values from the nested dictionary and convert them to a tuple
+    data_tuple = tuple(
+        [-value if col.startswith("-") else value for col, value in nested_data.items()]
+        + [datetime.now()]
+    )
 
-        # Extract the values from the nested dictionary and convert them to a tuple
-        data_tuple = tuple([-value if col.startswith('-') else value for col, value in value.items()] + [datetime.now()])
+    print("Data Tuple:", data_tuple)
+    # Execute the query
+    try:
+        cursor.execute(query, data_tuple)
+        connection.commit()
+        print("Data inserted successfully.")
+    except Exception as e:
+        print(f"Error inserting data: {e}")
 
-        # Execute the query
-        try:
-            cursor.execute(query, data_tuple)
-            connection.commit()
-            print("Data inserted successfully.")
-        except Exception as e:
-            print(f"Error inserting data: {e}")
+def insert_or_update_cumulative(table_name, nested_data, column_names):
+    # Wrap column names with backticks to handle special characters
+    columns = ", ".join(
+        [
+            f"`{col.replace('*', '_').replace('-', 'neg').replace('+', 'pos').replace('/', 'div').replace(' ', '_')}`"
+            for col in column_names
+            if col != "time"
+        ]
+    )
+
+    # Create values
+    values = ", ".join(["%s"] * len(column_names))
+    update_set = ", ".join(
+        [
+            f"{column} = VALUES({column})"
+            for column in column_names
+            if column != "time"
+        ]
+    )
+
+    # Include the 'time' column separately in values and update_set
+    columns += ", time"
+    values += ", %s"
+    update_set += ", time = VALUES(time)"
+
+    # Use ON DUPLICATE KEY UPDATE to handle updates
+    # query = f"INSERT INTO {table_name} ({columns}) VALUES ({values}) ON DUPLICATE KEY UPDATE {update_set}"
+    query = f"INSERT INTO cumulative (`S4_L2`, `S3_L1`, `S6_L2`, `S5_L1`, `S2_L2`, `S1_L1`, `S8_L2`, `S7_L1`, `S10_L2`, `S9_L1`, `S12_L2`, `S11_L1`, time) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) ON DUPLICATE KEY UPDATE S4_L2 = VALUES(S4_L2), S3_L1 = VALUES(S3_L1), S6_L2 = VALUES(S6_L2), S5_L1 = VALUES(S5_L1), S2_L2 = VALUES(S2_L2), S1_L1 = VALUES(S1_L1), S8_L2 = VALUES(S8_L2), S7_L1 = VALUES(S7_L1), S10_L2 = VALUES(S10_L2), S9_L1 = VALUES(S9_L1), S12_L2 = VALUES(S12_L2), S11_L1 = VALUES(S11_L1), time = VALUES(time)"
+
+    print("*******THIS IS MY QUERY ******\n")
+    print(query)
+    print("****THIS IS MY QUERY ******\n")
+    # Extract the values from the nested dictionary and convert them to a tuple
+    data_tuple = tuple(
+        [-value if col.startswith("-") else value for col, value in nested_data.items()]
+        + [datetime.now()]
+    )
+
+    print("Data Tuple:", data_tuple)
+    # Execute the query
+    try:
+        cursor.execute(query, data_tuple)
+        connection.commit()
+        print("Data inserted successfully.")
+    except Exception as e:
+        print(f"Error inserting data: {e}")
+
+
+
 
 # Check if the 'cumulative' table exists
 cursor.execute("SHOW TABLES LIKE 'cumulative'")
 table_exists = cursor.fetchone()
 # If the 'cumulative' table doesn't exist, create it
 if not table_exists:
-    cursor.execute("""
+    cursor.execute(
+        """
         CREATE TABLE cumulative (
-            S10_L2 DECIMAL(18, 6),
-            S7_L1 DECIMAL(18, 6),
-            S8_L2 DECIMAL(18, 6),
-            S9_L1 DECIMAL(18, 6),
-            S1_L1 DECIMAL(18, 6),
-            S11_L1 DECIMAL(18, 6),
-            S12_L2 DECIMAL(18, 6),
-            S2_L2 DECIMAL(18, 6),
-            S3_L1 DECIMAL(18, 6),
-            S4_L2 DECIMAL(18, 6),
-            S5_L1 DECIMAL(18, 6),
-            S6_L2 DECIMAL(18, 6),
+            S4_L2 DECIMAL(65, 30),
+            S3_L1 DECIMAL(65, 30),
+            S6_L2 DECIMAL(65, 30),
+            S5_L1 DECIMAL(65, 30),
+            S2_L2 DECIMAL(65, 30),
+            S1_L1 DECIMAL(65, 30),
+            S8_L2 DECIMAL(65, 30),
+            S7_L1 DECIMAL(65, 30),
+            S10_L2 DECIMAL(65, 30),
+            S9_L1 DECIMAL(65, 30),
+            S12_L2 DECIMAL(65, 30),
+            S11_L1 DECIMAL(65, 30),
             time TIMESTAMP,
             PRIMARY KEY (time)
         )
-    """)
-columns_list_cumulative = ['S10_L2', 'S7_L1', 'S8_L2', 'S9_L1', 'S1_L1', 'S11_L1', 'S12_L2', 'S2_L2', 'S3_L1', 'S4_L2', 'S5_L1', 'S6_L2']
+    """
+    )
+columns_list_cumulative = [
+    "S4_L2",
+    "S3_L1",
+    "S6_L2",
+    "S5_L1",
+    "S2_L2",
+    "S1_L1",
+    "S8_L2",
+    "S7_L1",
+    "S10_L2",
+    "S9_L1",
+    "S12_L2",
+    "S11_L1",
+]
 
 # Insert or update data in the 'cumulative' table
-insert_or_update('cumulative', cumulative, columns_list_cumulative + ['time'])
+insert_or_update_cumulative("cumulative", cumulative, columns_list_cumulative + ["time"])
 
 # Check if the 'rate' table exists
 cursor.execute("SHOW TABLES LIKE 'rate'")
@@ -198,29 +280,44 @@ table_exists = cursor.fetchone()
 
 # If the 'rate' table doesn't exist, create it
 if not table_exists:
-    cursor.execute("""
+    cursor.execute(
+        """
         CREATE TABLE rate (
-            S10_L2 DECIMAL(18, 6),
-            S7_L1 DECIMAL(18, 6),
-            S8_L2 DECIMAL(18, 6),
-            S9_L1 DECIMAL(18, 6),
-            S1_L1 DECIMAL(18, 6),
-            S11_L1 DECIMAL(18, 6),
-            S12_L2 DECIMAL(18, 6),
-            S2_L2 DECIMAL(18, 6),
-            S3_L1 DECIMAL(18, 6),
             S4_L2 DECIMAL(18, 6),
-            S5_L1 DECIMAL(18, 6),
+            S3_L1 DECIMAL(18, 6),
             S6_L2 DECIMAL(18, 6),
+            S5_L1 DECIMAL(18, 6),
+            S2_L2 DECIMAL(18, 6),
+            S1_L1 DECIMAL(18, 6),
+            S8_L2 DECIMAL(18, 6),
+            S7_L1 DECIMAL(18, 6),
+            S10_L2 DECIMAL(18, 6),
+            S9_L1 DECIMAL(18, 6),
+            S12_L2 DECIMAL(18, 6),
+            S11_L1 DECIMAL(18, 6),
             time TIMESTAMP,
             PRIMARY KEY (time)
         )
-    """)
+    """
+    )
 
-columns_list_rate = ['S10_L2', 'S7_L1', 'S8_L2', 'S9_L1', 'S1_L1', 'S11_L1', 'S12_L2', 'S2_L2', 'S3_L1', 'S4_L2', 'S5_L1', 'S6_L2']
+columns_list_rate = [
+    "S4_L2",
+    "S3_L1",
+    "S6_L2",
+    "S5_L1",
+    "S2_L2",
+    "S1_L1",
+    "S8_L2",
+    "S7_L1",
+    "S10_L2",
+    "S9_L1",
+    "S12_L2",
+    "S11_L1",
+]
 
 # Insert or update data in the 'rate' table
-insert_or_update('rate', rate, columns_list_rate + ['time'])
+insert_or_update_rate("rate", rate, columns_list_rate + ["time"])
 
 # Close the MySQL connection
 cursor.close()

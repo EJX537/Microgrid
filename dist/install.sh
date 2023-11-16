9b142be17396f4bf5c64847e894dd8760f2ef34f
EJX537 <67524385+EJX537@users.noreply.github.com>
2023-11-15 17:52:38 -0800

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -4,14 +4,7 @@
 sudo apt-get update
 
 # Install Docker
-sudo apt-get install -y docker.io
-
-# Install Docker Compose
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+sudo apt-get install -y docker docker-compose
 
 # Change to the /opt directory
 cd /opt

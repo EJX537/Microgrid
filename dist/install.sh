2e6c037d9e66b13e2ff65ddcd76178944c2b1216
EJX537 <67524385+EJX537@users.noreply.github.com>
2023-11-15 17:43:31 -0800

    Update install.sh

--- dist/install.sh
+++ dist/install.sh
@@ -7,14 +7,16 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
 # Change to the /opt directory
 cd /opt
 
 # Download the docker-compose.yaml file
-sudo curl -O https://raw.githubusercontent.com/EJX537/Microgrid/test-deployment/dist/docker-compose.yaml?token=GHSAT0AAAAAACIQRAAZF4U65XABKU6ZAYLSZKVPOQQ
-
+sudo curl -O https://raw.githubusercontent.com/EJX537/Microgrid/test-deployment/dist/docker-compose.yaml
 # Run the services
 sudo docker-compose up -d

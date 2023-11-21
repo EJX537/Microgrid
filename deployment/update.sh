3b1850338861eeceee722b4ec868b5d27705ea19
EJX537 <67524385+EJX537@users.noreply.github.com>
2023-11-20 20:09:27 -0800

    Update update.sh

--- deployment/update.sh
+++ deployment/update.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+echo "Taking down old docker containers"
+docker-compose down -v --remove-orphans
+
+# Wait until all containers are down
+while [ "$(docker-compose ps -q)" != "" ]; do
+  echo "Waiting for containers to stop..."
+  sleep 1
+done
+
 # Check if we are on a Windows system
+echo "Deleting old docker-compose.yaml"
 if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
 	# Change to the /Program\ Files directory
 	cd "\Program Files"
-	del /F /Q docker-compose.yaml
+	rm docker-compose.yaml
 
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	# Change to the /opt directory
@@ -19,7 +29,9 @@ else
 fi
 
 # Download the docker-compose.yaml file
-curl -O https://raw.githubusercontent.com/EJX537/Microgrid/test-deployment/dist/docker-compose.yaml
+echo "Downloading new docker-compose.yaml"
+curl -O -k https://raw.githubusercontent.com/EJX537/Microgrid/test-deployment/dist/docker-compose.yaml
 
+echo "Starting new docker containers"
 # Run the services
 docker-compose up -d
